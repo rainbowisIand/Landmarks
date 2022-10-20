5639efd55306d8b2a971c893f1b31d4fbcc3d637
Jinhwan Kim <hwanistic@gmail.com>
2022-10-20 12:43:29 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김진환 on 2022/10/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
